2ed313300b15de849fa2972d1f76fbe51fc8a97a
duanzhichao <duanzhichao2008@163.com>
2021-07-27 10:29:56 +0800

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -10,19 +10,19 @@ use Mix.Config
 #     environment variable DATABASE_URL is missing.
 #     For example: ecto://USER:PASS@HOST/DATABASE
 #     """
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+# secret_key_base =
+#   System.get_env("SECRET_KEY_BASE") ||
+#     raise """
+#     environment variable SECRET_KEY_BASE is missing.
+#     You can generate one by calling: mix phx.gen.secret
+#     """
 
 config :read_pg, ReadPgWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: "02kWiRb2hX1WLfm4/EfLCQHOWe/w0w2QVCSXkOgXJSxtA7lOZvehTUHTmykFemqd"
 
 # ## Using releases (Elixir v1.9+)
 #

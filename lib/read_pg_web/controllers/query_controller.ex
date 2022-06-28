1113713bc016e3dc7f1e4edbfcdcc9bcdfd25306
duanzhichao <duanzhichao2008@163.com>
2022-06-28 14:09:44 +0800

    同步代码

--- lib/read_pg_web/controllers/query_controller.ex
+++ lib/read_pg_web/controllers/query_controller.ex
@@ -13,73 +13,27 @@ defmodule ReadPgWeb.QueryController do
       true ->
         sql = Jason.decode!(sql)
           |> Enum.map(fn x -> Regex.replace(~r/get/, x, "select") end)
-          |> Enum.join(" ")
-
-        try do
-          IO.inspect sql
-          {:ok, pid} = Postgrex.start_link(hostname: "127.0.0.1", username: "postgres", password: "postgres", database: database)
-          {state, data} = Postgrex.query!(pid, sql, [], [timeout: 1500000000])
-          IO.inspect state
-          data = Postgrex.query!(pid, sql, [], [timeout: 1500000000])
-          GenServer.stop(pid)
-
-          length = length(data.columns) - 1
-
-          result = Enum.map(data.rows, fn xs ->
-              Enum.map(0..length, fn index -> {Enum.at(data.columns, index), Enum.at(xs, index)} end)
-              |> Map.new
-            end)
-          IO.inspect result
-          json conn, %{data: result}
-        rescue
-          _ ->
-          json conn, %{data: []}
-        end
-    end
-  end
-
-  def connect_test(conn, _params) do
-    time = case ReadPg.ets_get(:app, :start_time) do
-        nil -> ""
-        time -> time
-      enddefmodule ReadPgWeb.QueryController do
-  use ReadPgWeb, :controller
-
-  def query(conn, _params) do
-
-    %{"sql" => sql, "database" => database} = Map.merge(%{"sql" => "", "database" => "drg_prod"}, conn.params)
-
-    cond do
-      is_nil(sql) -> json conn, %{data: []}
-      sql == "" -> json conn, %{data: []}
-      database == "" -> json conn, %{data: []}
-      sql == [] -> json conn, %{data: []}
-      true ->
-        sql = Jason.decode!(sql)
-          |> Enum.map(fn x -> Regex.replace(~r/get/, x, "select") end)
           |> Enum.map(fn x -> Regex.replace(~r/edit/, x, "update") end)
           |> Enum.map(fn x -> Regex.replace(~r/add/, x, "create") end)
           |> Enum.join(" ")
 
         try do
-          IO.inspect sql
           {:ok, pid} = Postgrex.start_link(hostname: "127.0.0.1", username: "postgres", password: "postgres", database: database)
-          {state, data} = Postgrex.query!(pid, sql, [], [timeout: 1500000000])
-          IO.inspect state
+
           data = Postgrex.query!(pid, sql, [], [timeout: 1500000000])
           GenServer.stop(pid)
 
-          length = length(data.columns) - 1
-
           result = Enum.map(data.rows, fn xs ->
-              Enum.map(0..length, fn index -> {Enum.at(data.columns, index), Enum.at(xs, index)} end)
+              Enum.with_index(xs)
+              |> Enum.map(fn {v, i} -> {String.to_atom(Enum.at(data.columns, i)), v} end)
               |> Map.new
             end)
-          IO.inspect result
+
           json conn, %{data: result}
         rescue
-          _ ->
-          json conn, %{data: []}
+          error ->
+            IO.inspect error
+            json conn, %{data: []}
         end
     end
   end
@@ -89,11 +43,6 @@ defmodule ReadPgWeb.QueryController do
         nil -> ""
         time -> time
       end
-    json conn, %{data: time}
-  end
-
-end
-
     json conn, %{data: time}
   end
 
